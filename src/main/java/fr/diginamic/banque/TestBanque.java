94545ca0178494953b94d353ebada807128c0396
Sionra <farfadet.31@hotmail.fr>
2024-10-04 15:33:21 +0200

    TP 7

--- src/main/java/fr/diginamic/banque/TestBanque.java
+++ src/main/java/fr/diginamic/banque/TestBanque.java
@@ -1,11 +1,22 @@
 package fr.diginamic.banque;
 
 import fr.diginamic.banque.entites.Compte;
+import fr.diginamic.banque.entites.CompteTaux;
+
+import java.util.ArrayList;
 
 public class TestBanque {
     public static void main(String[] args) {
         Compte unCompte = new Compte(1, 324.54);
+        ArrayList<Compte> compteArray = new ArrayList<>();
 
-        System.out.println(unCompte);
+        compteArray.add(unCompte);
+        compteArray.add(new CompteTaux(2, 2948.23, 2.3));
+
+//        System.out.println(unCompte);
+        for (Compte val : compteArray){
+            System.out.println(val);
+            System.out.println("----------------------------------------");
+        }
     }
 }
---
+++ src/main/java/fr/diginamic/banque/entites/CompteTaux.java
@@ -0,0 +1,15 @@
+package fr.diginamic.banque.entites;
+
+public class CompteTaux extends Compte{
+    double taux;
+
+    public CompteTaux(int compteNum, double soldeCompte, double taux) {
+        super(compteNum, soldeCompte);
+        this.taux = taux;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nTaux de rémunération : " + this.taux + '%';
+    }
+}
